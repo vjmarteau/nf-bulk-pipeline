359a1d5e9f718e89cc0b4401cc3eec2a50311002
vjmarteau <valentinmarteau7@gmail.com>
2022-07-25 15:25:32 +0200

    Add main workflow params

--- run_bulk_pipeline.sh
+++ run_bulk_pipeline.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # Run custom bulk RNA-seq pipeline
 
-nextflow run ./main.nf \
--w /path/to/workDir/work \
---outdir /path/to/resDir/results \
---samplesheet /path/to/input/data/samplesheet.csv \
---gene_expression_matrix /path/to/input/data/gene_expression_matrix.tsv \
+nextflow run ./main.nf -profile cluster \
+-w /home/marteau/myScratch/nf-work-dir/nf-bulk-pipeline/work \
 -resume
